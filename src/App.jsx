ada8716bd1031371b5455f6bccc8391c10872ac6
Judith <infantejudith77@gmail.com>
2025-11-19 14:04:14 -0600

    I AM JIB

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,24 +10,15 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={"/assets/react.svg"} className="logo" alt="Vite logo" /> 
         </a>
-        <a href="https://rowdyhacks.org/_next/image?url=%2Fimg%2Fteam%2FJibril.webp&w=1080&q=100" target="_blank">
-          <img src="https://rowdyhacks.org/_next/image?url=%2Fimg%2Fteam%2FJibril.webp&w=1080&q=100" className="logo react" alt="React logo" />
+        <a href="/bwme.png" target="_blank">
+          <img src="/assets/bwme.png" className="logo react" alt="bwme" />
         </a>
       </div>
-      <h1>I AM JUDITH</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>I AM JIBRILLLLLLL</h1>
+
+
     </>
   )
 }
