2220d674176c9b39fce293702f967f4ad5d8af18
Judith <infantejudith77@gmail.com>
2026-03-02 00:18:37 -0600

    me

--- src/About.jsx
+++ src/About.jsx
@@ -16,6 +16,19 @@ export default function About() {
           <Typewriter text="Hello, I'm Judith!" />
         </div>
       </div>
+
+      <div className="about-text">
+        "I am a college student in my third year and majoring in computer science with a concentration in cybersecurity and cloud! I have an IT internship that has fostered a growing interest in the application of tech in the real-world solving real problems." 
+      </div>
+
+      <div className="comp-sci">
+        <img src="/assets/menjosie.jpg" alt="compsci" className="comp-sci-image" />
+        <img src="/assets/hackathon.jpg" alt="hackathon" className="hackathon-img" />
+      </div>
+
+      <div className = "digi">
+        <img src="/assets/digicam.PNG" alt="digi" className="digicam" />
+      </div>
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 export default function Home() {
   return (
-    <div className="home-page">
+    <div className="judith-face">
       <img src="/assets/bme3.PNG" alt="bme3" className="aura" />
     </div>
   );
